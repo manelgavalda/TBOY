111f0deb825d32af0bcf9b0ebce87b3e5bd6571d
Manel Gavaldà Andreu <manelgavalda@iesebre.com>
2017-05-17 21:42:03 +0200

    Map working?

--- assets/images/tiles.tsx
+++ assets/images/tiles.tsx
@@ -1,10 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tiles" tilewidth="32" tileheight="32" tilecount="25" columns="25" backgroundcolor="#3f3f3f">
+<tileset name="tileset1" tilewidth="32" tileheight="32" tilecount="25" columns="25">
  <image source="tiles.png" width="800" height="32"/>
- <terraintypes>
-  <terrain name="New Terrain" tile="0"/>
-  <terrain name="New Terrain" tile="1"/>
-  <terrain name="New Terrain" tile="2"/>
-  <terrain name="New Terrain" tile="3"/>
- </terraintypes>
 </tileset>
--- src/states/Game.js
+++ src/states/Game.js
@@ -10,15 +10,27 @@ export default class extends Phaser.State {
       // this.game.load.image('tiles', 'assets/tilesheet128.png');
 
   }
-
-
   create () {
 
-    let map = this.game.add.tilemap('level1');
-    this.map = map;
 
-      map.addTilesetImage('tilesetHouse', 'tiles');
-      map.addTilesetImage('Hints', 'hints');
+      this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
+      //Change the background colour
+      this.game.stage.backgroundColor = "#a9f0ff";
+
+      this.map = this.game.add.tilemap('tilemap');
+      this.map.addTilesetImage('tiles', 'tiles');
+
+      this.backgroundlayer = this.map.createLayer('backgroundLayer');
+      this.groundLayer = this.map.createLayer('blockedLayer');
+
+      //Before you can use the collide function you need to set what tiles can collide
+      this.map.setCollisionBetween(1, 10, true, 'blockedLayer');
+
+      //Change the world size to match the size of this layer
+      this.groundLayer.resizeWorld();
+
+
 
 
       // const bannerText = 'Phaser + ES6 + Webpack'
@@ -29,15 +41,15 @@ export default class extends Phaser.State {
     // banner.fill = '#77BFA3'
     // banner.smoothed = false
     // banner.anchor.setTo(0.5)
-
-    this.mushroom = new Mushroom({
-      game: this,
-      x: this.world.centerX,
-      y: this.world.centerY,
-      asset: 'mushroom'
-    })
-
-    this.game.add.existing(this.mushroom)
+    //
+    // this.mushroom = new Mushroom({
+    //   game: this,
+    //   x: this.world.centerX,
+    //   y: this.world.centerY,
+    //   asset: 'mushroom'
+    // })
+    //
+    // this.game.add.existing(this.mushroom)
   }
 
   render () {
