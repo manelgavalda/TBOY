c3717dff06f8396bc6ebb3056d52393677be8a34
manelgavalda <manelgavalda@iesebre.com>
2017-05-20 15:02:47 +0200

    Removed assets, addes map and player moving ok

--- assets/images/tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tileset1" tilewidth="32" tileheight="32" tilecount="25" columns="25">
- <image source="tiles.png" width="800" height="32"/>
-</tileset>
--- src/states/Game.js
+++ src/states/Game.js
@@ -1,56 +1,102 @@
 /* globals __DEV__ */
 import Phaser from 'phaser'
-import Mushroom from '../sprites/Mushroom'
+import Player from '../sprites/Player'
 
 export default class extends Phaser.State {
   init () {}
   preload () {
-      // this.game.load.spritesheet('player', 'assets/greenninja.png', 38, 48);
+      this.game.load.spritesheet('player', './assets/images/player.png', 96, 64)
+      // this.load.spritesheet('player', './assets/images/player.png', 38, 48);
       // this.game.load.tilemap('tilemap', 'assets/level.json', null, Phaser.Tilemap.TILED_JSON);
       // this.game.load.image('tiles', 'assets/tilesheet128.png');
 
   }
   create () {
+    this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
+    //Change the background colour
+    this.game.stage.backgroundColor = "#a9f0ff";
 
-      this.game.physics.startSystem(Phaser.Physics.ARCADE);
+    this.map = this.game.add.tilemap('tilemap');
+    this.map.addTilesetImage('tiles', 'tiles');
 
-      //Change the background colour
-      this.game.stage.backgroundColor = "#a9f0ff";
-
-      this.map = this.game.add.tilemap('tilemap');
-      this.map.addTilesetImage('tiles', 'tiles');
-
-      this.backgroundlayer = this.map.createLayer('backgroundLayer');
-      this.groundLayer = this.map.createLayer('blockedLayer');
-
-      //Before you can use the collide function you need to set what tiles can collide
-      this.map.setCollisionBetween(1, 100, true, 'blockedLayer');
-
-      //Change the world size to match the size of this layer
-      this.groundLayer.resizeWorld();
-
-
-
-
-      // const bannerText = 'Phaser + ES6 + Webpack'
-    // let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText)
-    // banner.font = 'Bangers'
-    // banner.padding.set(10, 16)
-    // banner.fontSize = 40
-    // banner.fill = '#77BFA3'
-    // banner.smoothed = false
-    // banner.anchor.setTo(0.5)
+    this.backgroundLayer = this.map.createLayer('background');
+    // this.groundLayer = this.map.createLayer('ground');
     //
-    this.mushroom = new Mushroom({
-      game: this,
-      x: this.world.centerX,
-      y: this.world.centerY,
-      asset: 'mushroom'
-    })
+    // //Before you can use the collide function you need to set what tiles can collide
+    this.map.setCollisionBetween(1, 100, true, 'background');
 
-    this.game.add.existing(this.mushroom)
+    //Change the world size to match the size of this layer
+    this.backgroundLayer.resizeWorld();
+
+
+    this.player = this.game.add.sprite(250, 101, 'player')
+    this.game.physics.arcade.enable(this.player)
+    this.game.physics.arcade.enable(this.backgroundLayer)
+    this.player.body.gravity.y = 0
+
+    this.player.body.setSize(20, 20)
+
+    this.cursor = game.input.keyboard.createCursorKeys()
+
+
+    this.player.frame = 1
+    this.player.animations.add('down', [0, 2], 2, true)
+    this.player.animations.add('left', [3, 4, 5], 5, true)
+    this.player.animations.add('right', [6, 7, 8], 8, true)
+    this.player.animations.add('up', [9, 10, 11], 11, true)
   }
+
+  update(){
+      this.game.physics.arcade.collide(this.player, this.backgroundLayer)
+
+
+
+      this.inputs()
+      // if (this.player.body) {
+      //     if (this.player.body.touching.down) {
+      //         if (this.hasJumped) {
+      //             this.dustSound.play();
+      //             this.dust.x = this.player.x;
+      //             this.dust.y = this.player.y + 10;
+      //             this.dust.start(true, 300, null, 8);
+      //         }
+      //         this.hasJumped = false
+      //     }
+      // }
+  }
+
+    inputs () {
+        if(this.cursor.down.isDown) {
+            this.player.animations.play('down')
+            this.player.body.velocity.y = +220
+        } else {
+            this.player.body.velocity.y = 0
+        }
+
+        if (this.cursor.left.isDown) {
+            this.player.animations.play('left')
+            this.player.body.velocity.x = -220
+        } else {
+            this.player.body.velocity.x = 0
+        }
+
+        if (this.cursor.right.isDown) {
+            this.player.animations.play('right')
+            this.player.body.velocity.x = +220
+        }
+
+        if(this.cursor.up.isDown) {
+            this.player.animations.play('up')
+            this.jumpPlayer()
+        }
+    }
+
+    jumpPlayer () {
+        if (!this.hasJumped) {
+            this.player.body.velocity.y = -220
+        }
+    }
 
   render () {
     if (__DEV__) {
