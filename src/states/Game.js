db96bafb5e346b21e7e7a7b404b0feed638c8940
Manel Gavaldà Andreu <manelgavalda@iesebre.com>
2017-05-30 21:00:40 +0200

    Adding virtual inputs

--- src/states/Game.js
+++ src/states/Game.js
@@ -56,6 +56,9 @@ export default class extends Phaser.State {
         this.createBullets();
 
         this.game.camera.follow(this.player);
+
+
+
         this.game.camera.setSize(800, 500);
 
         // Create a missile and add it to the game in the bottom center of the stage
@@ -242,6 +245,56 @@ export default class extends Phaser.State {
         {
             this.player.animations.stop();
         }
+
+        // create our virtual game controller buttons
+        this.buttonjump = this.game.add.button(600, 500, 'up', null, this, 0, 1, 0, 1);  //game, x, y, key, callback, callbackContext, overFrame, outFrame, downFrame, upFrame
+        this.buttonjump.fixedToCamera = true;  //our buttons should stay on the same place
+        this.buttonjump.events.onInputOver.add(function(){jump=true;});
+        this.buttonjump.events.onInputOut.add(function(){jump=false;});
+        this.buttonjump.events.onInputDown.add(function(){jump=true;});
+        this.buttonjump.events.onInputUp.add(function(){jump=false;});
+
+        // buttonfire = this.game.add.button(700, 500, 'up', null, this, 0, 1, 0, 1);
+        // buttonfire.fixedToCamera = true;
+        // buttonfire.events.onInputOver.add(function(){fire=true;});
+        // buttonfire.events.onInputOut.add(function(){fire=false;});
+        // buttonfire.events.onInputDown.add(function(){fire=true;});
+        // buttonfire.events.onInputUp.add(function(){fire=false;});
+        //
+        // buttonleft = this.game.add.button(0, 472, 'buttonhorizontal', null, this, 0, 1, 0, 1);
+        // buttonleft.fixedToCamera = true;
+        // buttonleft.events.onInputOver.add(function(){left=true;});
+        // buttonleft.events.onInputOut.add(function(){left=false;});
+        // buttonleft.events.onInputDown.add(function(){left=true;});
+        // buttonleft.events.onInputUp.add(function(){left=false;});
+        //
+        // buttonbottomleft = this.game.add.button(32, 536, 'buttondiagonal', null, this, 6, 4, 6, 4);
+        // buttonbottomleft.fixedToCamera = true;
+        // buttonbottomleft.events.onInputOver.add(function(){left=true;duck=true;});
+        // buttonbottomleft.events.onInputOut.add(function(){left=false;duck=false;});
+        // buttonbottomleft.events.onInputDown.add(function(){left=true;duck=true;});
+        // buttonbottomleft.events.onInputUp.add(function(){left=false;duck=false;});
+        //
+        // buttonright = this.game.add.button(160, 472, 'buttonhorizontal', null, this, 0, 1, 0, 1);
+        // buttonright.fixedToCamera = true;
+        // buttonright.events.onInputOver.add(function(){right=true;});
+        // buttonright.events.onInputOut.add(function(){right=false;});
+        // buttonright.events.onInputDown.add(function(){right=true;});
+        // buttonright.events.onInputUp.add(function(){right=false;});
+        //
+        // buttonbottomright = this.game.add.button(160, 536, 'buttondiagonal', null, this, 7, 5, 7, 5);
+        // buttonbottomright.fixedToCamera = true;
+        // buttonbottomright.events.onInputOver.add(function(){right=true;duck=true;});
+        // buttonbottomright.events.onInputOut.add(function(){right=false;duck=false;});
+        // buttonbottomright.events.onInputDown.add(function(){right=true;duck=true;});
+        // buttonbottomright.events.onInputUp.add(function(){right=false;duck=false;});
+        //
+        // buttondown = this.game.add.button(96, 536, 'buttonvertical', null, this, 0, 1, 0, 1);
+        // buttondown.fixedToCamera = true;
+        // buttondown.events.onInputOver.add(function(){duck=true;});
+        // buttondown.events.onInputOut.add(function(){duck=false;});
+        // buttondown.events.onInputDown.add(function(){duck=true;});
+        // buttondown.events.onInputUp.add(function(){duck=false;});
     }
 
     enemyFollow() {
